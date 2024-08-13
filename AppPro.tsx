541e60cabebbb6f0d8df09ede9d205dd2187e6a7
venkatesh reddy <venkateshpunganur@gmail.com>
2024-08-13 16:11:42 -0700

    reactnative

---
+++ AppPro.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+    View,Text,StyleSheet
+} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+
+
+export default function AppPro(){
+    return(
+        <View>
+            <Text>
+                Hi There 
+            </Text>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+        textColor:{
+            color:white
+        },
+        
+    
+})
